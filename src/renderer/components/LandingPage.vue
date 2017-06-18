9bbd44cee19584731859c7c810d67d8371076c98
gengjiawen <technicalcute@gmail.com>
2017-06-19 00:14:49 +0800

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="wrapper">
+    <button @click="download">Click me to test dugite, check output in Console</button>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+  import {GitProcess} from 'dugite'
+  import os from 'os'
+  import fs from 'fs-extra'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+      download () {
+        const desDir = os.tmpdir() + Date.now()
+        console.log(desDir)
+        fs.ensureDirSync(desDir)
+        GitProcess.exec(['clone', 'https://github.com/desktop/dugite', desDir])
+          .then(result => {
+            console.log('response')
+            if (result.exitCode === 0) {
+              console.log('congratulations')
+              const output = result.stdout
+              console.log(output)
+            } else {
+              const error = result.stderr
+              console.log(error)
+            }
+          })
+          .catch(e => {
+            console.log('error')
+            console.log(e)
+          })
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+</style>
